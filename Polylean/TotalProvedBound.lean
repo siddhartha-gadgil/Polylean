bcce59e55c5c77900deaf7e064ab8b547675d561
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2022-01-06 12:24:34 +0530

    notation tweak

--- Polylean/TotalProvedBound.lean
+++ Polylean/TotalProvedBound.lean
@@ -39,19 +39,19 @@ def totalProvedBound(n: Nat) : (w: Word) → (w.length ≤ n) →  ProvedBound w
       have lem : 0 < List.length (x :: ys) := by apply Nat.succ_pos
       have lem2: 0 < 0 := Nat.lt_of_lt_of_le lem wit
       exact False.elim (Nat.lt_irrefl 0 lem2)
-  | Nat.succ j =>
+  | m + 1 =>
     match w with
     | [] => fun _ => ProvedBound.emptyWord
     | x :: ys => fun wit =>
-      have lem : ys.length ≤ j := by
+      have lem : ys.length ≤ m := by
           have lm : List.length (x :: ys) = ys.length + 1 := rfl
           rw [lm] at wit
           apply Nat.le_of_succ_le_succ
           exact wit
-      let head := ProvedBound.prepend x (totalProvedBound j ys lem)
+      let head := ProvedBound.prepend x (totalProvedBound m ys lem)
       let splits := provedSplits x⁻¹  ys
       let tail := splits.map (fun ps => 
-        let fstBound : ps.fst.length ≤ j := by
+        let fstBound : ps.fst.length ≤ m := by
               have lm := splitFirst ps
               apply Nat.le_of_succ_le_succ
               apply Nat.le_trans lm
@@ -60,7 +60,7 @@ def totalProvedBound(n: Nat) : (w: Word) → (w.length ≤ n) →  ProvedBound w
               apply Nat.le_of_succ_le_succ
               apply Nat.le_trans wit
               apply Nat.le_succ
-        let sndBound : ps.snd.length ≤ j := by
+        let sndBound : ps.snd.length ≤ m := by
               have lm := splitSecond ps
               apply Nat.le_of_succ_le_succ
               apply Nat.le_trans lm
@@ -70,10 +70,10 @@ def totalProvedBound(n: Nat) : (w: Word) → (w.length ≤ n) →  ProvedBound w
               apply Nat.le_trans wit
               apply Nat.le_succ
         ProvedBound.headMatches x ys ps.fst ps.snd ps.proof 
-          (totalProvedBound j ps.fst fstBound ) 
-          (totalProvedBound j ps.snd sndBound ))
+          (totalProvedBound m ps.fst fstBound ) 
+          (totalProvedBound m ps.snd sndBound ))
       ProvedBound.min head tail
 
 #eval (totalProvedBound 5 ([α, α, β, α!, β!]) (by decide)).bound
 
-#eval (totalProvedBound 10 ([α, α, β, α!, β!]^2) (by decide) ).bound
+#eval (totalProvedBound 10 ([α, α, β, α!, β!]^2) (by decide)).bound
