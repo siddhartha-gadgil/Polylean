6d62fc2e3584c58ba516d545291d8d49ac893adb
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2022-04-29 17:06:53 +0530

    memo length calling itself

--- Polylean/MemoLength.lean
+++ Polylean/MemoLength.lean
@@ -14,11 +14,12 @@ def memoLength : Word → IO Nat := fun w => do
     | [] => return 0
     | x :: ys => do
       let base := 1 + (← memoLength ys)
-      let derived := (splits x⁻¹ ys).map fun ⟨(fst, snd), h⟩ =>
-      have h : fst.length + snd.length < ys.length + 1 := Nat.lt_trans h (Nat.lt_succ_self _)
-      have h1 : snd.length < ys.length + 1  := Nat.lt_of_le_of_lt (Nat.le_add_left _ _) h
-      have h2 : fst.length < ys.length + 1 := Nat.lt_of_le_of_lt (Nat.le_add_right _ _) h
-      length fst + length snd
+      let derived ←  (splits x⁻¹ ys).mapM fun ⟨(fst, snd), h⟩ => do
+        have h : fst.length + snd.length < ys.length + 1 := Nat.lt_trans h (Nat.lt_succ_self _)
+        have h1 : snd.length < ys.length + 1  := Nat.lt_of_le_of_lt (Nat.le_add_left _ _) h
+        have h2 : fst.length < ys.length + 1 := Nat.lt_of_le_of_lt (Nat.le_add_right _ _) h
+        return (← memoLength fst) + (← memoLength snd)
       let res := derived.foldl min base -- minimum of base and elements of derived
       normCache.set <| cache.insert w res
       return res
+termination_by _ l => l.length
