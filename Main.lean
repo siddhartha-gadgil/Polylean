81b3d8d50c6fc74d453951405c5bdc046b0157d4
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2022-04-29 18:22:28 +0530

    main uses 7

--- Main.lean
+++ Main.lean
@@ -5,7 +5,7 @@ import Polylean.MemoLength
 open Letter
 
 def main : IO Unit := do
-  let n := 8
+  let n := 7
   let w := [α, α, β, α!, β!]^n
   let l ← memoLength w 
   IO.println s!"Length of [α, α, β, α!, β!]^{n}, {l}"
