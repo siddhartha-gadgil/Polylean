caa34effc7e5d4b9432565efd2e68146d61fdc3d
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2022-05-02 11:10:26 +0530

    some speedup: custom hash, length from back

--- Main.lean
+++ Main.lean
@@ -5,8 +5,14 @@ import Polylean.ProvedBound
 import Polylean.MemoLength
 open Letter
 
-def main : IO Unit := do
-  let n := 7
+def main(args: List String) : IO Unit := do
+  let n : Nat := 
+    match args.head? with
+    | none => 7 
+    | some s =>
+      match s.toNat? with
+      | some n => n
+      | none => 7
   let w := [α, α, β, α!, β!]^n
   let l ← wordLength w 
   IO.println s!"Length of [α, α, β, α!, β!]^{n}, {l}"
--- Polylean/Length.lean
+++ Polylean/Length.lean
@@ -15,10 +15,12 @@ def Word.wrd (w: Word) : Wrd := w.toArray.map <| fun l => l.int
 
 universe u
 
-instance{α : Type u}[Hashable α] : Hashable (Array α) :=
-  ⟨fun arr => hash arr.data⟩
-
 namespace Wrd
+
+def hashfn (w: Wrd) : UInt64 := 
+  w.foldl (fun h i => mixHash h (hash i)) 7
+
+instance : Hashable Wrd := ⟨hashfn⟩
 
 initialize normCache : IO.Ref (HashMap Wrd Nat) ← IO.mkRef (HashMap.empty)
 
@@ -26,10 +28,10 @@ def splits(w: Wrd)(l : Int) : IO <| Array (Wrd × Wrd) := do
   let mut tail := w.eraseIdx 0
   let mut init : Wrd := #[]
   let mut accum : Array (Wrd × Wrd) := #[]
-  for j in [0:w.size] do
-    if l = w[j] then
+  for x in w do
+    if l = x then
       accum := accum.push (init, tail)
-    init := init.push w[j]
+    init := init.push x
     tail := tail.eraseIdx 0
   return accum
 
@@ -44,9 +46,9 @@ do
       match w.size with
       | 0 => pure 0
       | k + 1 => do
-        let mut l := 1 + (← length <| w.eraseIdx 0)
-        let x := w[0]
-        let ys := w.eraseIdx 0
+        let ys := w.pop
+        let x := w.back
+        let mut l := 1 + (← length <| ys)
         let pairs ←  splits ys (-x)
         for (fst, snd) in pairs do
           let pl := (← length <| fst) + (← length <| snd)
@@ -62,3 +64,5 @@ def wordLength(w: Word):IO Nat :=
   Wrd.length <| Word.wrd w
 
 #eval (Word.wrd ([α, α, β, α!, α,  β!])).splits (1)
+
+#check Array.reverse
