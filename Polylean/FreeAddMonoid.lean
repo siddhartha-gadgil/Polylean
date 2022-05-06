cb5b433455dc528d59739260829c7ef72bb5ac5d
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2022-05-06 18:05:12 +0530

    main technical coefficient result: half +

--- Polylean/FreeAddMonoid.lean
+++ Polylean/FreeAddMonoid.lean
@@ -5,7 +5,7 @@ a free "Nat-module" with basis X.
 
 variable (X: Type)[DecidableEq X]
 
-def FormalSum : Type := List (Nat × X)
+abbrev FormalSum : Type := List (Nat × X)
 
 inductive basicRel : FormalSum X  → FormalSum X   →  Prop where
 | zeroCoeff (tail: FormalSum X)(x: X)(a : Nat)(h: a = 0):  
@@ -18,7 +18,37 @@ inductive basicRel : FormalSum X  → FormalSum X   →  Prop where
         basicRel  ((a₁, x₁) :: (a₂, x₂) :: tail) 
                     ((a₂, x₂) :: (a₁, x₁) :: tail)
 
-def NatSpan := Quot (@basicRel X)
+def NatSpan := Quot (basicRel X)
+
+def FormalSum.sum {X: Type} [DecidableEq X] (s: FormalSum X): NatSpan X :=
+      Quot.mk (basicRel X) s
+
+def FormalSum.equiv {X: Type} [DecidableEq X] (s₁ s₂: FormalSum X): Prop :=
+      s₁.sum = s₂.sum
+
+infix:65 " ≅ " => FormalSum.equiv
+
+theorem consEquiv{X: Type}[DecidableEq X] (s₁ s₂ : FormalSum X) (a: Nat) (x: X):
+      s₁ ≅ s₂ → (a, x) :: s₁ ≅ (a, x) :: s₂  := by
+        intro h
+        let f : FormalSum X → NatSpan X := 
+            fun s => FormalSum.sum <| (a, x) :: s
+        let wit : (s₁ s₂ : FormalSum X) → (basicRel X s₁ s₂) → f s₁ = f s₂ :=
+            by
+            intro s₁ s₂ hyp
+            apply Quot.sound
+            apply basicRel.cons
+            assumption
+        let g := Quot.lift f wit
+        let factorizes : 
+            (s : FormalSum X) → g (s.sum) = 
+              FormalSum.sum ((a, x) :: s) := Quot.liftBeta f wit
+        rw[FormalSum.equiv]
+        rw [← factorizes]
+        rw [← factorizes]
+        rw [h]
+
+             
 
 def monoCoeff {X: Type}[DecidableEq X](x₀ : X) (nx : Nat × X) : Nat := 
   match  (nx.2 == x₀) with 
@@ -35,22 +65,73 @@ theorem monoCoeffZero (x₀ x : X) : monoCoeff x₀ (0, x) = 0 :=
       rw [monoCoeff]
       cases x==x₀ <;> rfl
 
-def coeffSum(x₀ : X) : FormalSum X → Nat
+def FormalSum.coeff{X: Type}[DecidableEq X](x₀ : X) : FormalSum X → Nat
 | [] => 0
-| h :: l => (monoCoeff x₀ h) + (coeffSum x₀ l)
+| h :: l => (monoCoeff x₀ h) + (coeff x₀ l)
 
 open basicRel in
 theorem coeff_well_defined (x₀ : X)(s₁ s₂: FormalSum X)(h: basicRel X s₁ s₂):
-        coeffSum X x₀  s₁ = coeffSum X x₀ s₂ := by
+        FormalSum.coeff  x₀  s₁ = FormalSum.coeff  x₀ s₂ := by
           induction h with
-          | zeroCoeff tail x a hyp => simp [coeffSum, hyp, monoCoeffZero]
+          | zeroCoeff tail x a hyp => simp [FormalSum.coeff, hyp, monoCoeffZero]
           | addCoeffs a b x tail => 
-            simp [coeffSum, monoCoeffZero, ← Nat.add_assoc, monoCoeffHom]
+            simp [FormalSum.coeff, monoCoeffZero, ← Nat.add_assoc, monoCoeffHom]
           | cons a x s₁ s₂ r step => 
-            simp [coeffSum, step]
+            simp [FormalSum.coeff, step]
           | swap a₁ a₂ x₁ x₂ tail => 
-            simp [coeffSum, ← Nat.add_assoc, Nat.add_comm]
+            simp [FormalSum.coeff, ← Nat.add_assoc, Nat.add_comm]
 
 def NatSpan.coeff (x₀ : X) : NatSpan X → Nat := 
-      Quot.lift (coeffSum X x₀) (coeff_well_defined X x₀)
+      Quot.lift (FormalSum.coeff  x₀) (coeff_well_defined X x₀)
 
+theorem coeffComplement (x₀ : X)(s : FormalSum X) :
+        0 < s.coeff x₀  → 
+          (∃ ys: FormalSum X, 
+            (((s.coeff x₀, x₀) :: ys) ≅ s) ∧ 
+            (List.length ys < s.length))  := by 
+            induction s with 
+            | nil =>
+              intro contra
+              have l : FormalSum.coeff x₀ [] = 0 := by rfl
+              rw [l] at contra
+              let l' := Nat.not_succ_le_zero _ contra  
+              exact False.elim l'
+            | cons head tail hyp => 
+              let (a, x) := head
+              intro pos
+              cases c:x == x₀ with
+              | true => 
+                  admit
+              | false =>
+                let k := FormalSum.coeff x₀ tail
+                have lem : FormalSum.coeff x₀ tail = 
+                      FormalSum.coeff x₀ ((a, x) :: tail) := 
+                        by 
+                          rw [FormalSum.coeff, monoCoeff]
+                          rw [c]
+                          simp [Nat.zero_add]
+                rw [← lem] at pos          
+                let ⟨ys', eqnStep, lIneqStep⟩ := hyp pos   
+                rw [← lem]
+                let ys := (a, x) :: ys'      
+                have lIneq : ys.length < ((a, x) :: tail).length := 
+                  by 
+                    have d : ys.length = ys'.length + 1 := by rfl
+                    have d' : ((a, x) :: tail).length = tail.length + 1 := 
+                        by rfl
+                    rw [d, d']
+                    apply Nat.succ_lt_succ
+                    exact lIneqStep
+                have eqn₁ : 
+                  (k, x₀) :: ys ≅ (a, x) :: (k, x₀) :: ys' := by
+                    apply Quot.sound 
+                    apply basicRel.swap
+                have eqn₂ : 
+                  (a, x) :: (k, x₀) :: ys' ≅ (a, x) :: tail := 
+                    by 
+                      apply consEquiv
+                      assumption 
+                have eqn : (k, x₀) :: ys ≅ (a, x) :: tail := by 
+                    exact Eq.trans eqn₁ eqn₂
+                exact ⟨ys, eqn, lIneq⟩
+termination_by _ s => s.length
