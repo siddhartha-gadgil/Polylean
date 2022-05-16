03384f15a69aac7ded80fa7aba7ba71dfeb98f35
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2022-05-16 11:48:49 +0530

    added mathlib4 dependency

--- lakefile.lean
+++ lakefile.lean
@@ -2,5 +2,9 @@ import Lake
 open Lake DSL
 
 package polylean {
-  -- add configuration options here
+  dependencies := #[{
+    name := "mathlib",
+    src := Source.git "https://github.com/leanprover-community/mathlib4.git" "9c54c410ebcd03ebecb4f8b0e510735088dd5412"
+  }],
+
 }
