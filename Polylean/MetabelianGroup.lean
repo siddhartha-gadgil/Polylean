d6f36ce6ac8e39cd5e71d093a945f91131281e39
ART0 <18333981+0Art0@users.noreply.github.com>
2022-05-18 15:52:43 +0530

    Cocycle condition

--- Polylean/MetabelianGroup.lean
+++ Polylean/MetabelianGroup.lean
@@ -1,3 +1,5 @@
+import Polylean.GroupAction
+
 /-
 Metabelian groups are group extensions `1 → K → G → Q → 1` with both the kernel and the quotient abelian. Such an extension is determined by data:
 
@@ -6,3 +8,10 @@ Metabelian groups are group extensions `1 → K → G → Q → 1` with both the
 
 We have to define the cocycle condition and construct a group structure on a structure extending `K × Q`. The main step is to show that the cocyle condition implies associativity.
 -/
+
+class Cocycle {Q K : Type _} [AddCommGroup Q] [AddCommGroup K] [α : AddCommGroup.ActionByAutomorphisms Q K]
+  (c : Q → Q → K) where
+  leftId : ∀ q : Q, c 0 q = (0 : K)
+  rightId : ∀ q : Q, c q 0 = (0 : K)
+  invEq : ∀ q : Q, c q (-q) = c (-q) q
+  cocycleCondition : ∀ q q' q'' : Q, c q q' + c (q + q') q'' = q • c q' q'' + c q (q' + q'')
