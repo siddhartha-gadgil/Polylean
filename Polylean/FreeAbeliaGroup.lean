94ffcd2be3c949d952c9161541449e66df9b6f0c
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2022-05-19 22:55:32 +0530

    partially done hom from Z

--- Polylean/FreeAbeliaGroup.lean
+++ Polylean/FreeAbeliaGroup.lean
@@ -9,6 +9,55 @@ variable {A : Type} [abg : AddCommGroup A]
 def f (a: A) : ℤ → A := 
   fun n => abg.gsmul n a
 
+theorem gsmul_succ (n: ℤ) (x : A) : gsmul (n+1) x = x + gsmul n x  := by 
+    cases n  
+    case ofNat k => 
+      apply abg.gsmul_succ'
+    case negSucc k => 
+      induction k with
+      | zero => 
+        simp
+        simp [add_zero]
+        have l : -[1+ 0] + 1 = 0 := by rfl
+        rw [l]
+        have l₀ : gsmul 0 x = 0 := by apply abg.gsmul_zero'
+        rw [l₀]
+        simp
+        rw [abg.gsmul_neg']
+        simp
+        let l : gsmul 1 x = 
+                x + gsmul (0) x := abg.gsmul_succ' 0 x
+        rw [l]
+        rw [l₀]
+        simp
+      | succ l ih => 
+        have l₀ : -[1+ Nat.succ l] + 1 = -[1+ l] := by rfl
+        rw [l₀]
+        rw [abg.gsmul_neg']
+        rw [abg.gsmul_neg']
+        simp
+
+        let l₁ := abg.gsmul_succ' (l + 1) x
+        simp at l₁
+        rw [l₁]
+        simp
+        let y := gsmul (l + 1) x
+        show -y = x + -(x + y)
+        apply Eq.symm
+        conv =>
+          lhs
+          arg 2
+          rw [add_comm]
+        
+        admit
+
+
+
+        
+#check add_left_cancel
+    
+#check neg_add_self
+
 theorem isHomPos (x : A) (n m: Nat) : f x (n + m) = f x n + f x m :=
   by 
     induction m with
@@ -22,6 +71,7 @@ theorem isHomPos (x : A) (n m: Nat) : f x (n + m) = f x n + f x m :=
       rw [← add_assoc]
       simp
       let l₁ := abg.gsmul_succ' k x
+
       simp at l₁
       rw [l₁]
       simp
