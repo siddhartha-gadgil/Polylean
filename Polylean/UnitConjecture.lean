0deec44da8b2f286921d042ba685b8c94000e5ba
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2022-05-30 09:22:37 +0530

    debugging steps

--- Polylean/UnitConjecture.lean
+++ Polylean/UnitConjecture.lean
@@ -32,26 +32,40 @@ def q : RP := (x⁻¹*y⁻¹ : RP) + x + y⁻¹*z + z
 def r: RP := one + x + y⁻¹*z + x*y*z
 def s : RP  := one + x*z⁻¹ + x⁻¹*z⁻¹ + y*z⁻¹ + y⁻¹*z⁻¹
 
-def poly := p + q * a + r * b + s * a * b
-
+def α := p + q * a + r * b + s * a * b
+ 
 def p' : RP := x⁻¹ * (a⁻¹ * p * a)
 def q' : RP := -x⁻¹ * q
 def r' : RP := -y⁻¹ * r
 def s' : RP := z⁻¹ * (a⁻¹ * s * a)
 
-def poly' := p' + q' * a + r' * b + s' * a * b
+def α' := p' + q' * a + r' * b + s' * a * b
+
+#eval α' * α -- not 1
+
+#eval (a * b)⁻¹ * p * (a * b) = x⁻¹ * y⁻¹ * p -- true
+#eval (a * b)⁻¹ * q * (a * b) = y * q -- true
+#eval (a * b)⁻¹ * r * (a * b) = x⁻¹  * r -- true
+#eval (a * b)⁻¹ * s * (a * b) = s -- true
+
+#eval b⁻¹ * p * b = x⁻¹ * y * (a⁻¹ * p * a) -- true
+#eval b⁻¹ * q * b = y⁻¹  * (a⁻¹ * q * a) -- true
+#eval b⁻¹ * r * b = x⁻¹  * (a⁻¹ * r * a) -- true
+#eval b⁻¹ * s * b = (a⁻¹ * s * a) -- true
+
 
 #eval a
 #eval a * a
 #eval q
+#eval (y, a⁻¹ * y * a)
 #eval q * a 
 #eval (x * a)
-#eval poly
-#eval poly'
-#eval poly * poly' 
+#eval α
+#eval α'
+
 #eval q
 #eval q'
 #eval x⁻¹
 #eval y
 #eval a⁻¹ * y * a
-#eval p
+
