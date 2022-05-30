cc1f2d0be50395a50c694920189c5027baf8f853
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2022-05-30 10:28:28 +0530

    some corrections

--- Polylean/UnitConjecture.lean
+++ Polylean/UnitConjecture.lean
@@ -32,14 +32,14 @@ def q : RP := (x⁻¹*y⁻¹ : RP) + x + y⁻¹*z + z
 def r: RP := one + x + y⁻¹*z + x*y*z
 def s : RP  := one + x*z⁻¹ + x⁻¹*z⁻¹ + y*z⁻¹ + y⁻¹*z⁻¹
 
-def α := p + q * a + r * b + s * a * b
+def α := p + (q * a) + (r * b) + (s * a * b)
  
-def p' : RP := x⁻¹ * (a⁻¹ * p * a)
-def q' : RP := -x⁻¹ * q
-def r' : RP := -y⁻¹ * r
-def s' : RP := z⁻¹ * (a⁻¹ * s * a)
+def p' : RP := x⁻¹ * (a * p * a⁻¹)
+def q' : RP := -(x⁻¹ * q)
+def r' : RP := -(y⁻¹ * r)
+def s' : RP := z⁻¹ * (a* s * a⁻¹)
 
-def α' := p' + q' * a + r' * b + s' * a * b
+def α' := p' + (q' * a) + (r' * b) + (s' * a * b)
 
 #eval α' * α -- not 1
 
@@ -48,12 +48,22 @@ def α' := p' + q' * a + r' * b + s' * a * b
 #eval (a * b)⁻¹ * r * (a * b) = x⁻¹  * r -- true
 #eval (a * b)⁻¹ * s * (a * b) = s -- true
 
-#eval b⁻¹ * p * b = x⁻¹ * y * (a⁻¹ * p * a) -- true
-#eval b⁻¹ * q * b = y⁻¹  * (a⁻¹ * q * a) -- true
-#eval b⁻¹ * r * b = x⁻¹  * (a⁻¹ * r * a) -- true
-#eval b⁻¹ * s * b = (a⁻¹ * s * a) -- true
+#eval b * p * b⁻¹ = x⁻¹ * y * (a * p * a⁻¹) -- true
+#eval b * q * b⁻¹ = y⁻¹  * (a * q * a⁻¹) -- true
+#eval b * r * b⁻¹ = x⁻¹  * (a * r * a⁻¹) -- true
+#eval b * s * b⁻¹ = (a * s * a⁻¹) -- true
 
+def lhs₁ : RP := (x⁻¹ * (a *p * a⁻¹) * q) - (x⁻¹ *  q * (a * p * a⁻¹)) - (x⁻¹ * z⁻¹ * y⁻¹ * r * (a * s  * a⁻¹)) + (y⁻¹ * z⁻¹ * (a * s * a⁻¹) * x⁻¹ * r) 
+#eval lhs₁ -- [] ; correct
+def lhsTerms : List RP :=  [(x⁻¹ * (a *p * a⁻¹) * q), - (x⁻¹ *  q * (a * p * a⁻¹)), - (x⁻¹ * z⁻¹ * y⁻¹ * r * (a * s  * a⁻¹)),  (y⁻¹ * z⁻¹ * (a * s * a⁻¹) * x⁻¹ * r)] 
+#eval lhsTerms 
+def lhsMulA  := lhsTerms.map (fun x : RP => x * a)
+#eval (p' * q) ∈ lhsTerms -- true
+#eval (p' * q * a) ∈ lhsMulA -- true
+#eval (q' * a * p) ∈ lhsMulA -- true
+#eval (r' * b * s * a * b) ∈ lhsMulA
 
+#eval a * b * a
 #eval a
 #eval a * a
 #eval q
