d1cef421b73484442c04e8b41553f7c43b6775ef
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2022-05-30 19:11:27 +0530

    more docs

--- Polylean/SMul.lean
+++ Polylean/SMul.lean
@@ -1,3 +1,4 @@
+/-- Scalar multiplication, specialized form of `HMul` for using a different symbol and easier type inference -/
 class SMul (α : Type u) (β : Type v)  where
   sMul : α → β → β 
 
--- Polylean/UnitConjecture.lean
+++ Polylean/UnitConjecture.lean
@@ -2,12 +2,13 @@ import Polylean.GardamGroup
 import Polylean.GroupRing
 
 /-
-The proof of the theorem `𝔽₂[P]` has non-trivial units.
+The proof of the theorem `𝔽₂[P]` has non-trivial units. Together with the main result of `TorsionFree` -- that `P` is torsion-free, this completes the formal proof of Gardam's theorem that Kaplansky's Unit Conjecture is false.
 -/
 
-
+/-- definition of a unit -/
 def unit {R : Type _} [Ring R] (u : R) := ∃ v : R, v * u = (1 : R)
 
+/-- definition of being trivial, i.e., of the form `a⬝g` for `g` a group element and `a ≠ 0`-/
 def trivial_element {R G : Type _} [CommRing R] [DecidableEq R] [Group G] [DecidableEq G] (x : FreeModule R G) : Prop :=
   ∃ g : G, ¬(FreeModule.coordinates g x = 0) ∧ (∀ h : G, ¬(FreeModule.coordinates h x = 0) → h = g)
 
@@ -27,27 +28,28 @@ abbrev a : P := ((0, 0, 0), P.a)
 abbrev b : P := ((0, 0, 0), P.b)
 abbrev one : RP := 1
 
+/-! The components of the non-trivial unit `α` -/
 def p : RP := one +  x +  y +  x*y +  z⁻¹ + x*z⁻¹ + y*z⁻¹ + x*y*z⁻¹
 def q : RP := (x⁻¹*y⁻¹ : RP) + x + y⁻¹*z + z
 def r: RP := one + x + y⁻¹*z + x*y*z
 def s : RP  := one + x*z⁻¹ + x⁻¹*z⁻¹ + y*z⁻¹ + y⁻¹*z⁻¹
 
+/-- the non-trivial unit `α` -/
 def α := p + (q * a) + (r * b) + (s * a * b)
  
+/-! The components of the inverse `α'` of the non-trivial unit `α` -/
 def p' : RP := x⁻¹ * (a⁻¹  * p * a)
 def q' : RP := -(x⁻¹ * q)
 def r' : RP := -(y⁻¹ * r)
 def s' : RP := z⁻¹ * (a⁻¹ * s * a)
 
+/-- the inverse `α'` of the non-trivial unit `α` -/
 def α' := p' + (q' * a) + (r' * b) + (s' * a * b)
 
-#eval α
-#eval α.coordinates (-z)
-#eval α.coordinates (x * y)
-
-
+/-- `α` is a unit -/
 theorem is_unit : unit α := ⟨α', by native_decide⟩
 
+/-- `α` is  non-trivial -/
 theorem non_trivial : ¬ (trivial_element α) := by
     intro contra
     let ⟨g, p⟩ := contra
@@ -62,6 +64,7 @@ theorem non_trivial : ¬ (trivial_element α) := by
     have ineq : -z ≠  x * y := by native_decide
     contradiction
 
+/-- the existence of a non-trivial unit in `P` -/
 theorem Gardam : ∃ g : RP, unit g ∧ ¬ (trivial_element g) := 
   ⟨α, And.intro is_unit non_trivial⟩
 
