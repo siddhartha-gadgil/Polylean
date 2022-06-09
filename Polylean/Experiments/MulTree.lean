ed6e0e63a330b9d9089797452fec32338372437f
ART0 <18333981+0Art0@users.noreply.github.com>
2022-06-09 12:26:23 +0530

    Add `reduceStep` function

--- Polylean/Experiments/MulTree.lean
+++ Polylean/Experiments/MulTree.lean
@@ -45,7 +45,19 @@ def MulTree.fold {α : Type u}[HMul α α α][Repr α]  (t : MulTree α ) : α :
   match t with
   | MulTree.leaf a => a
   | MulTree.node l r =>  (fold l) * (fold r)
-  
+
+def MulTree.leafCount {α : Type _} [HMul α α α] [Repr α] : MulTree α → ℕ
+  | MulTree.leaf _ => Nat.zero.succ
+  | MulTree.node l r => leafCount l + leafCount r
+
+def MulTree.left {α : Type _} [HMul α α α] [Repr α] : MulTree α → α
+  | MulTree.leaf lf => lf
+  | MulTree.node l _ => left l
+
+def MulTree.right {α : Type _} [HMul α α α] [Repr α] : MulTree α → α
+  | MulTree.leaf lf => lf
+  | MulTree.node _ r => right r
+
 partial def treeM (e : Expr) : MetaM Expr := do
   match ← mul? e with
   | none  => mkAppM ``MulTree.leaf #[e]
@@ -67,3 +79,34 @@ def egTree(n m: Nat)  := tree# ((n * m) * (m + 2))
 def egTreeInv(n m: Nat) := (egTree n m).fold
 
 example (n m: Nat) : egTreeInv n m = (n * m) * (m + 2) := rfl
+
+
+
+section treeexperiments
+
+def t₁ : MulTree Nat := -- tree# (2 * (3 * 5))
+ MulTree.node (MulTree.leaf 2) (MulTree.node (MulTree.leaf 3) (MulTree.leaf 5))
+
+def t₂ : MulTree Nat :=
+  MulTree.node (MulTree.leaf 6) (MulTree.leaf 5)
+
+theorem prodeq : 2 * (3 * 5) = 6 * 5 := rfl
+
+theorem prodred : 6 * 5 = 30 := rfl
+
+theorem foldeq : t₁.fold = t₂.fold := rfl
+
+end treeexperiments
+
+open MulTree in
+def Group.reduce {G : Type _} [Group G] [DecidableEq G] [Repr G] : (mt : MulTree G) → {μt : MulTree G // mt.fold = μt.fold}
+  | leaf lf => ⟨leaf lf, rfl⟩
+  | node l r =>
+    if h:(l.right * r.left = (1 : G)) then
+      match l, r with
+        | leaf lf, leaf rf => ⟨leaf (1 : G), by simp [left, right] at h; simp [MulTree.fold]; exact h⟩
+        | node ll lr, r => ⟨node ll (node lr r), by simp [MulTree.fold]; rw [mul_assoc]⟩
+        | l, node rl rr => ⟨node (node l rl) rr, by simp [MulTree.fold]; rw [mul_assoc]⟩
+    else
+      match reduce l, reduce r with
+        | ⟨l_, eql⟩, ⟨r_, eqr⟩ => ⟨node l_ r_, by simp [MulTree.fold]; rw [eql, eqr]⟩
