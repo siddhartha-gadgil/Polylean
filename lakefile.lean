0cf9e832bbbf059f2a2700283b2adda69b492e88
ART0 <18333981+0Art0@users.noreply.github.com>
2022-06-18 15:54:53 +0530

    Add aesop dependency

--- lakefile.lean
+++ lakefile.lean
@@ -5,6 +5,8 @@ package polylean {
   dependencies := #[{
     name := "mathlib",
     src := Source.git "https://github.com/leanprover-community/mathlib4.git" "9c54c410ebcd03ebecb4f8b0e510735088dd5412"
-  }],
-
+  },
+  { name := `aesop
+    src := Source.git "https://github.com/JLimperg/aesop" "3d7ae7fa4a1ddb3ae0448d82d65c0acf2e031351" }
+],
 }
