710eecbeec70fe64ad555cb7b3b866b1d96ed279
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2022-07-26 19:37:02 +0530

    started better docs

--- GardamTheorem.lean
+++ GardamTheorem.lean
@@ -1,3 +1,10 @@
 import Polylean.UnitConjecture
 import Polylean.TorsionFree
 
+/-!
+The proof of Gardam's theorem. The two parts are:
+
+* Torsion-freeness of the group.
+* The existence of non-trivial units : `Gardam`.
+
+-/
