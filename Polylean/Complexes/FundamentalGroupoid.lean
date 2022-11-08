61214e1692b0c9b6be44568f38c78a62f1489226
ART0 <18333981+0Art0@users.noreply.github.com>
2022-11-08 13:41:56 +0530

    Rename `Relator` to `NullHomotopy`

--- Polylean/Complexes/FundamentalGroupoid.lean
+++ Polylean/Complexes/FundamentalGroupoid.lean
@@ -1,23 +1,17 @@
-import Polylean.Complexes.TwoComplex
+import Polylean.Complexes.Structures.CombinatorialTwoComplex
+import Polylean.Complexes.Structures.Groupoid
 import Mathlib.Algebra.Group.Defs
 
 /-- The fundamental groupoid of a two-complex. -/
-instance FundamentalGroupoid {V : Type _} (CV : TwoComplex V) : Groupoid V where
+instance FundamentalGroupoid {V : Sort _} (CV : CombinatorialTwoComplex V) : Groupoid V where
   hom := λ u v => Quotient $ Path.Homotopy.setoid u v
   id := λ v => .mk' .nil
   comp := .lift₂ (λ p q => .mk' $ .append p q) $ λ _ _ _ _ h h' => 
             Quotient.sound $ Path.Homotopy.mul_sound h h'
   inv := .lift (λ p => .mk' p.inverse) λ _ _ h => 
             Quotient.sound $ Path.Homotopy.inv_sound h
-  invInv := by
-    intro _ _
-    apply Quotient.ind
-    intro e
-    apply Quotient.sound
-    simp
-    apply Path.Homotopy.refl
 
-  idComp := by
+  id_comp := by
     intro _ _
     apply Quotient.ind
     intro
@@ -25,7 +19,7 @@ instance FundamentalGroupoid {V : Type _} (CV : TwoComplex V) : Groupoid V where
     rw [Path.nil_append]
     apply Path.Homotopy.refl
 
-  compId := by
+  comp_id := by
     intro _ _
     apply Quotient.ind
     intro
@@ -33,7 +27,7 @@ instance FundamentalGroupoid {V : Type _} (CV : TwoComplex V) : Groupoid V where
     rw [Path.append_nil]
     apply Path.Homotopy.refl
 
-  compAssoc := by
+  comp_assoc := by
     intro _ _ _ _ f g h
     revert f
     apply Quotient.ind
@@ -48,14 +42,14 @@ instance FundamentalGroupoid {V : Type _} (CV : TwoComplex V) : Groupoid V where
     rw [Path.append_assoc]
     apply Path.Homotopy.refl        
 
-  invComp := by
+  inv_comp_id := by
     intro _ _
     apply Quotient.ind
     intro
     apply Quotient.sound
     apply Path.Homotopy.inv_cancel_left
 
-  compInv := by
+  comp_inv_id := by
     intro _ _
     apply Quotient.ind
     intro
@@ -63,15 +57,15 @@ instance FundamentalGroupoid {V : Type _} (CV : TwoComplex V) : Groupoid V where
     apply Path.Homotopy.inv_cancel_right
 
 /-- The group of endomorphisms at an object of a groupoid. -/
-def Groupoid.groupAt {G : Type _} (Gpd : Groupoid G) (g : G) : Group (g ⟶ g) where
+def Groupoid.groupAt {G : Sort _} (Gpd : Groupoid G) (g : G) : Group (g ⟶ g) where
   mul := Gpd.comp
   one := Gpd.id g
   inv := Gpd.inv
 
-  mul_assoc := Gpd.compAssoc
-  mul_one := Gpd.compId
-  one_mul := Gpd.idComp
-  mul_left_inv := Gpd.invComp
+  mul_assoc := Gpd.comp_assoc
+  mul_one := Gpd.comp_id
+  one_mul := Gpd.id_comp
+  mul_left_inv := Gpd.inv_comp_id
 
   npow_zero' := by intros; rfl
   npow_succ' := by intros; rfl
@@ -83,7 +77,7 @@ def Groupoid.groupAt {G : Type _} (Gpd : Groupoid G) (g : G) : Group (g ⟶ g) w
   gpow_succ' := by intros; rfl
 
 /-- The fundamental group of a two-complex. -/
-@[instance] def FundamentalGroup {V : Type _} (CV : TwoComplex.{_, _ + 1} V) (v : V) := 
+@[instance] def FundamentalGroup {V : Sort _} (CV : CombinatorialTwoComplex.{_ + 1, _ + 1} V) (v : V) := 
   @Groupoid.groupAt V (FundamentalGroupoid CV) v
 
 /-- A group as a single-object groupoid. -/
@@ -92,19 +86,9 @@ instance Group.toGroupoid {G : Type } (_ : Group G) : Groupoid Unit where
   id := fun | _ => (1 : G)
   comp := λ g h => g * h
   inv := λ g => g⁻¹
-  invInv := by simp
 
-  idComp := one_mul
-  compId := mul_one
-  compAssoc := mul_assoc
-  invComp := mul_left_inv
-  compInv := mul_right_inv
-
-#check @Path.compose
-
--- does not work
-instance Groupoid.toTwoComplex {G : Type _} (Grpd : Groupoid G) : AbstractTwoComplex G where
-  G := Grpd.toInvertegory.toSerreGraph
-  H := Grpd
-  collapse := @Path.compose G Grpd.toInvertegory.toCategory
-  collapseId := sorry
+  id_comp := one_mul
+  comp_id := mul_one
+  comp_assoc := mul_assoc
+  inv_comp_id := mul_left_inv
+  comp_inv_id := mul_right_inv
