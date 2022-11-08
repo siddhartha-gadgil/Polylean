a7ada8a662ce06393bb37577617121420324d206
ART0 <18333981+0Art0@users.noreply.github.com>
2022-11-08 14:51:05 +0530

    Fix `Invertegory` code (soon to be purged)

--- Polylean/Complexes/Structures/Invertegory.lean
+++ Polylean/Complexes/Structures/Invertegory.lean
@@ -12,11 +12,3 @@ structure Invertegory.Functor {C D : Sort _} (ℭ : Invertegory C) (𝔇 : Inver
   mapInv : {X Y : C} → {f : X ⟶ Y} → map (Invertegory.inv f) = Invertegory.inv (map f)
 
 attribute [simp] Invertegory.Functor.mapInv
-
-@[simp] protected def Invertegory.Functor.id (C : Sort _) [ℭ : Invertegory C] : Invertegory.Functor ℭ ℭ :=
--- TODO Use `..` notation
- { obj := id, map := id, mapId := λ _ => rfl, mapComp := λ _ _ => rfl, mapInv := rfl }
-
-@[simp] def Invertegory.Functor.comp {C D E : Sort _} {ℭ : Invertegory C} {𝔇 : Invertegory D} {𝔈 : Invertegory E} (F : Invertegory.Functor ℭ 𝔇) (G : Invertegory.Functor 𝔇 𝔈) : Invertegory.Functor ℭ 𝔈 :=
--- TODO Use `..` notation
-  { obj := G.obj ∘ F.obj, map := G.map ∘ F.map, mapId := by intro; simp, mapComp := by intros; simp, mapInv := by intros; simp }
