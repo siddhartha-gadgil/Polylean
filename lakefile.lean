6203ac610cc4102c831a4afb8a51d6bd368d8ba4
ART0 <18333981+0Art0@users.noreply.github.com>
2022-11-09 13:21:41 +0530

    Remove fixed package versions from `lakefile`

--- lakefile.lean
+++ lakefile.lean
@@ -15,7 +15,4 @@ lean_exe polymath
 lean_lib GardamTheorem
 
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git"@"a4369ab32fb20b9bd33f5064032ff2b2e2f67abe"
-
-require std from git
-  "https://github.com/leanprover/std4"@"735cbeae0fe896a7029d616b651e6a71e27653f8"
+  "https://github.com/leanprover-community/mathlib4.git"@"master"
