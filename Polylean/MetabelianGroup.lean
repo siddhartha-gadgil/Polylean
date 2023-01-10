09807451ebf25d0d750d8057b67c6081c1ca56aa
ART0 <18333981+0Art0@users.noreply.github.com>
2023-01-10 13:18:20 +0530

    Simplify `MetabelianGroup` (WIP)

--- Polylean/MetabelianGroup.lean
+++ Polylean/MetabelianGroup.lean
@@ -1,60 +1,59 @@
-import Polylean.Morphisms
-import Polylean.GroupAction
-import Polylean.Tactics.ReduceGoal
+import Mathlib.Algebra.Hom.Group
+import Polylean.AutAction
 
-/-
+/-!
 Metabelian groups are group extensions `1 → K → G → Q → 1` with both the kernel and the quotient abelian. Such an extension is determined by data:
 
 * a group action of `Q` on `K`
 * a cocyle `c: Q → Q → K`
 
-We have to define the cocycle condition and construct a group structure on a structure extending `K × Q`. The main step is to show that the cocyle condition implies associativity.
+We define the cocycle condition and construct a group structure on a structure extending `K × Q`. The main step is to show that the cocyle condition implies associativity.
 -/
 
 
-/-!
+/--
 A cocycle associated with a certain action of `Q` on `K` via automorphisms is a function from `Q × Q` to `K` satisfying
 a certain requirement known as the "cocycle condition". This allows one to define an associative multiplication operation on the set `K × Q` as shown below.
 The requirement `c 0 0 = (0 : K)` is not strictly necessary and mainly for convenience.
 -/
 class Cocycle {Q K : Type _} [AddCommGroup Q] [AddCommGroup K] (c : Q → Q → K) where
-  α : Q → K → K
-  [autaction : AutAction Q K α]
-  cocycleId : c 0 0 = (0 : K)
-  cocycleCondition : ∀ q q' q'' : Q, c q q' + c (q + q') q'' = q • c q' q'' + c q (q' + q'')
+  /-- An action of the quotient on the kernel by automorphisms. -/
+  α : Q → (K →+ K)
+  /-- A typeclass instance for the action by automorphisms. -/
+  [autAct : AutAction Q K α]
+  /-- The value of the cocycle is zero when its inputs are zero, as a convention. -/
+  cocycle_zero : c 0 0 = (0 : K)
+  /-- The *cocycle condition*. -/
+  cocycle_condition : ∀ q q' q'' : Q, c q q' + c (q + q') q'' = q +ᵥ c q' q'' + c q (q' + q'')
 
 namespace Cocycle
 
-/-
+/-!
 A few deductions from the cocycle condition.
 -/
 
 variable {Q K : Type _} [AddCommGroup Q] [AddCommGroup K]
 variable (c : Q → Q → K) [ccl : Cocycle c]
 
-attribute [simp] Cocycle.cocycleId
+attribute [simp] Cocycle.cocycle_zero
 
-instance : AutAction Q K ccl.α := ccl.autaction
+instance : AutAction Q K ccl.α := ccl.autAct
 
-@[simp] theorem leftId : ∀ {q : Q}, c 0 q = (0 : K) := by
-  intro q
-  have := Eq.symm $ ccl.cocycleCondition 0 0 q
-  simp at this; assumption
+@[simp] theorem left_id {q : Q} : c 0 q = (0 : K) := by
+  have := ccl.cocycle_condition 0 0 q
+  aesop
 
-@[simp] theorem rightId : ∀ {q : Q}, c q 0 = (0 : K) := by
-  intro q
-  have := ccl.cocycleCondition q 0 0
-  simp at this; assumption
+@[simp] theorem right_id {q : Q} : c q 0 = (0 : K) := by
+  have := ccl.cocycle_condition q 0 0
+  aesop
 
-theorem invRel : ∀ q : Q, c q (-q) = q • (c (-q) q) := by
-  intro q
-  have := ccl.cocycleCondition q (-q) q
-  simp at this; assumption
+theorem inv_rel (q : Q) : c q (-q) = q +ᵥ (c (-q) q) := by
+  have := ccl.cocycle_condition q (-q) q
+  aesop
 
-theorem invRel' : ∀ q : Q, c (-q) q = (-q) • (c q (-q)) := by
-  intro q
-  have := invRel c (-q)
-  simp at this; assumption
+theorem inv_rel' (q : Q) : c (-q) q = (-q) +ᵥ (c q (-q)) := by
+  have := inv_rel c (-q)
+  aesop
 
 end Cocycle
 
@@ -63,44 +62,33 @@ namespace MetabelianGroup
 variable {Q K : Type _} [AddCommGroup Q] [AddCommGroup K]
 variable (c : Q → Q → K) [ccl : Cocycle c]
 
-/-
+/--
 The multiplication operation defined using the cocycle.
 The cocycle condition is crucially used in showing associativity and other properties.
 -/
-@[reducible] def mul : (K × Q) → (K × Q) → (K × Q)
-  | (k, q), (k', q') => (k + (q • k') + c q q', q + q')
+@[reducible, aesop norm unfold] def mul : (K × Q) → (K × Q) → (K × Q)
+  | (k, q), (k', q') => (k + (q +ᵥ k') + c q q', q + q')
 
-def e : K × Q := (0, 0)
+@[reducible, aesop norm unfold] def e : K × Q := (0, 0)
 
-@[reducible] def inv : K × Q → K × Q
-  | (k, q) => (- ((-q) • (k  + c q (-q))), -q)
+@[reducible, aesop norm unfold] def inv : K × Q → K × Q
+  | (k, q) => (- ((-q) +ᵥ (k  + c q (-q))), -q)
 
-theorem left_id : ∀ (g : K × Q), mul c e g = g
-  | (k, q) => by simp [e, mul]
+@[simp] theorem left_id : ∀ (g : K × Q), mul c e g = g
+  | (k, q) => by aesop
 
-theorem right_id : ∀ (g : K × Q), mul c g e = g
-  | (k, q) => by 
-        rw [e, mul]
-        show (k + q • 0 + c q 0, q + 0) = (k, q)
-        have cq0 : c q 0 = 0 := by apply Cocycle.rightId
-        rw [cq0]
-        have q0 : q • (0 : K) = 0 := 
-          by apply AutAction.act_zero
-        rw[q0]
-        repeat (rw [add_zero])
-
+@[simp] theorem right_id : ∀ (g : K × Q), mul c g e = g
+  | (k, q) => by aesop
 
 theorem left_inv : ∀ (g : K × Q), mul c (inv c g) g = e
-  | (k , q) => by
-    simp [mul, inv, e]
-    rw [← Cocycle.invRel']
-    rw [add_comm (-(-q • k)) _, add_assoc, add_assoc, ← add_assoc _ (-q • k) _]; simp
+  | (k , q) => by sorry
 
 theorem right_inv : ∀ (g : K × Q), mul c g (inv c g) = e
   | (k, q) => by
-    simp [mul, inv, e]
+    reduceGoal
     repeat (rw [← AddCommGroup.Action.compatibility])
-    simp;
+    simp
+    sorry
 
 theorem mul_assoc : ∀ (g g' g'' : K × Q), mul c (mul c g g') g'' =  mul c g (mul c g' g'')
   | (k, q), (k', q'), (k'', q'') => by
@@ -108,11 +96,11 @@ theorem mul_assoc : ∀ (g g' g'' : K × Q), mul c (mul c g g') g'' =  mul c g (
     apply And.intro
     · rw [add_assoc, add_assoc, add_assoc, add_assoc, add_assoc, add_left_cancel_iff,
          add_assoc, add_left_cancel_iff, ← add_assoc, ← add_assoc, add_comm (c q q') _, add_assoc, add_assoc, add_left_cancel_iff]
-      exact ccl.cocycleCondition q q' q'' -- the cocycle condition implies associativity
+      exact ccl.cocycle_condition q q' q'' -- the cocycle condition implies associativity
     · rw [add_assoc]
 
 -- A proof that `K × Q` can be given a group structure using the above multiplication operation
-instance metabeliangroup : Group (K × Q) :=
+instance metabelianGroup : Group (K × Q) :=
   {
     mul := mul c,
     mul_assoc := mul_assoc c,
@@ -124,22 +112,15 @@ instance metabeliangroup : Group (K × Q) :=
     mul_left_inv := left_inv c,
 
     div_eq_mul_inv := by intros; rfl
-
-    npow_zero' := by intros; rfl,
-    npow_succ' := by intros; rfl,
-
-    -- gpow_zero' := by intros; rfl,
-    -- gpow_succ' := by intros; rfl,
-    -- gpow_neg' := by intros; rfl,
   }
 
-def mult (k k' : K) (q q' : Q) : (k, q) * (k', q') =  (k + (q • k') + c q q', q + q') := rfl
+def mul_eq (k k' : K) (q q' : Q) : (k, q) * (k', q') = (k + (q +ᵥ k') + c q q', q + q') := rfl
 
-def inverse (k : K) (q : Q) : (k, q)⁻¹ = (- ((-q) • (k  + c q (-q))), -q) := rfl
+def inv_eq (k : K) (q : Q) : (k, q)⁻¹ = (- ((-q) +ᵥ (k  + c q (-q))), -q) := rfl
 
 end MetabelianGroup
 
-
+/-
 section Exactness
 
 /-
@@ -152,7 +133,7 @@ This isomorphism is later used in proving that `P` is torsion-free from the fact
 variable (Q K : Type _) [AddCommGroup Q] [AddCommGroup K]
 variable (c : Q → Q → K) [ccl : Cocycle c]
 
-instance G : Group (K × Q) := MetabelianGroup.metabeliangroup c
+instance G : Group (K × Q) := MetabelianGroup.metabelianGroup c
 
 -- this is the subgroup of the metabelian group that occurs as
 -- the image of the inclusion of `K` and the kernel of the projection onto `Q`.
@@ -207,7 +188,7 @@ instance : AddCommGroup.Homomorphism (Metabelian.Kernel.projection Q K) where
     -- show (Metabelian.Kernel.projection Q K) ⟨(k, (0 : Q)) * (k', (0 : Q)), _⟩ = k + k'
     show (fun (k, _) => k) ((k, (0 : Q)) * (k', (0 : Q))) = k + k'
     reduceGoal
-    -- show k + (0 : Q) • k' + c 0 0 = k + k'
+    -- show k + (0 : Q) +ᵥ k' + c 0 0 = k + k'
     simp
 
 instance : AddCommGroup.Isomorphism K (Metabelian.Kernel Q K) :=
@@ -229,3 +210,4 @@ instance : AddCommGroup.Isomorphism K (Metabelian.Kernel Q K) :=
   }
 
 end Exactness
+-/
